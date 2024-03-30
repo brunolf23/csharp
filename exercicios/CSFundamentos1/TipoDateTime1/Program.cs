b4a254d7c3456eac5a06fa2450dc48d4ccffc553
Bruno <bruno.lf@hotmail.com>
2024-03-29 22:30:23 -0300

    objetos e datetime

---
+++ exercicios/CSFundamentos1/TipoDateTime1/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Struct DateTime");
+
+DateTime dataAtual = DateTime.Now;
+
+Console.WriteLine(dataAtual);
+
+// criar uma data especifica usa formato: aaaa/mm/dd
+DateTime dataHoje = new DateTime(2024, 03, 29);
+Console.WriteLine(dataHoje);
+
+// definir as horas
+DateTime dataHoraHoje = new DateTime(2024, 03, 29, 22, 16, 23);
+Console.WriteLine(dataHoraHoje);
+
+// extraindo informações de data e horário
+DateTime hoje = DateTime.Now;
+Console.WriteLine($"Hoje: {hoje}\n");
+
+Console.WriteLine(hoje.Day);
+Console.WriteLine(hoje.Month);
+Console.WriteLine(hoje.Year);
+Console.WriteLine(hoje.Hour);
+Console.WriteLine(hoje.Minute);
+Console.WriteLine(hoje.Second);
+Console.WriteLine(hoje.Millisecond);
+
+// adicionando valores
+Console.WriteLine(hoje.AddDays(60));
+Console.WriteLine(hoje.AddMonths(3));
+Console.WriteLine(hoje.AddYears(2));
+Console.WriteLine(hoje.AddHours(500));
+
+// obter o dia da semana e do ano
+Console.WriteLine(hoje.DayOfWeek);
+Console.WriteLine(hoje.DayOfYear);
+
+Console.ReadKey();
---
+++ exercicios/CSFundamentos1/TiposDeDados4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Definindo variáveis do tipo string e object e dynamic");
+Console.WriteLine();
+
+string nome = "Curso de C#";
+String titulo = "Curso de C# Avançado"; // System.String / uma forma de usar do .net
+
+Console.WriteLine(nome);
+Console.WriteLine(titulo);
+
+string valor = "Isto é uma string";
+valor = "Isto é uma string alterada";
+valor = "Alterando pela terceira vez";
+
+Console.WriteLine(valor);
+
+// usamos o object como um coringa, quando não temos informações sobre os dados
+object nota = 10;
+object valor1 = 8.55m;
+object nome1 = "Maria";
+object ativa = true;
+object letra = "A";
+
+Console.WriteLine(nota);
+Console.WriteLine(valor1);
+Console.WriteLine(nome1);
+Console.WriteLine(ativa);
+Console.WriteLine(letra);
+
+Console.ReadLine();
