6179d82792e1366690cfe2c8de121e00fe6dd6ff
Bruno <bruno.lf@hotmail.com>
2024-04-01 22:46:12 -0300

    identificadores

---
+++ exercicios/CSFundamentos1/Identificadores/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Definindo Identificadores");
+Console.WriteLine();
+
+// identificadores válidos
+string nome;
+string nomeCompleto;
+string idade;
+string _valor;
+int idade1;
+
+// identificadores inválidos
+//int 5idade;
+//int $valor;
+//int valor#total;
+//string nome Completo;
+
+// para nomes de variaveis camel case
+string nomeCompleto2;
+string oMaiorValor;
+string desconto_Total;
+
+// para uma constantes : MAIÚSCULAS
+double PI = 3.14;
+double PREFIXO = 21;
+string PREFIXO_22 = "21";
+
+Console.ReadKey();
