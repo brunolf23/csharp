7d12c2d4f31086ade81c3f9cfd9dece8e7eb9ce5
Bruno <bruno.lf@hotmail.com>
2024-04-04 18:51:05 -0300

    treinando

---
+++ microsoft/ProjetoTeste3/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+string[] novosProdutos = ["Geleia", "Manteiga", "Queijo", "Presunto", "Mortadela"];
+
+object contador = 0;
+foreach (string s in novosProdutos)
+{
+    Console.WriteLine($"Desejo o que no pão? {s}");
+}
+*/
+
+
+string[] dados = ["Bruno", "Lopes", "Bruno", "Lopes"];
+int contador = 1;
+
+foreach (string s in dados)
+{
+    Console.WriteLine($"O {s} foi contado {contador} vez");
+    contador += 1;
+}
