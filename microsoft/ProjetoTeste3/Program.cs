bfdd8a8c50f6ca2fc8f9f33038ab9ac44f66bfdf
Bruno Lopes <=>
2024-04-05 23:58:14 -0300

    contador

--- microsoft/ProjetoTeste3/Program.cs
+++ microsoft/ProjetoTeste3/Program.cs
@@ -17,3 +17,20 @@ foreach (string s in dados)
     Console.WriteLine($"O {s} foi contado {contador} vez");
     contador += 1;
 }
+
+int idade = 18;
+if (idade >= 18)
+{
+    // Se a váriavel idade for maior ou igual à 18 esse trecho de código será executado
+    Console.WriteLine("Você é maior de idade!");
+}
+else if (idade >= 16)
+{
+    // Se a váriavel idade não for maior ou igual à 18 mas for maior ou igual a 16 esse trecho de código será executado
+    Console.WriteLine("Você não é maior de idade, mas já pode votar!");
+}
+else
+{
+    // Se a váriavel idade não for maior ou igual à 18 esse trecho de código será executado
+    Console.WriteLine("Você é menor de idade!");
+}
