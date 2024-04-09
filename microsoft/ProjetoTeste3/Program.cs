90170c97796e163d4ec1986cf22e66b5faaa176e
Bruno Lopes <=>
2024-04-08 21:33:24 -0300

    calculadora v1

--- microsoft/ProjetoTeste3/Program.cs
+++ microsoft/ProjetoTeste3/Program.cs
@@ -1,39 +1,114 @@
-﻿/*
-string[] novosProdutos = ["Geleia", "Manteiga", "Queijo", "Presunto", "Mortadela"];
+﻿using System;
 
-object contador = 0;
-foreach (string s in novosProdutos)
+namespace Calculator
 {
-    Console.WriteLine($"Desejo o que no pão? {s}");
-}
-*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
 
+        static void Menu()
+        {
+            Console.Clear();
 
-string[] dados = ["Bruno", "Lopes", "Bruno", "Lopes"];
-int contador = 1;
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Sair");
 
-foreach (string s in dados)
-{
-    Console.WriteLine($"O {s} foi contado {contador} vez");
-    contador += 1;
-}
+            Console.WriteLine("----------");
+            Console.WriteLine("Selecione uma opção: ");
 
-int idade = 18;
-if (idade >= 18)
-{
-    // Se a váriavel idade for maior ou igual à 18 esse trecho de código será executado
-    Console.WriteLine("Você é maior de idade!");
-}
-else if (idade >= 16)
-{
-    // Se a váriavel idade não for maior ou igual à 18 mas for maior ou igual a 16 esse trecho de código será executado
-    Console.WriteLine("Você não é maior de idade, mas já pode votar!");
-}
+            short res = short.Parse(Console.ReadLine());
 
+            switch (res)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
+        }
 
+        static void Soma()
+        {
+            Console.Clear();
 
-else
-{
-    // Se a váriavel idade não for maior ou igual à 18 esse trecho de código será executado
-    Console.WriteLine("Você é menor de idade!");
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 + v2;
+            // Console.WriteLine("O resultado da soma é " + resultado);
+            Console.WriteLine($"O resultado da soma é {resultado}");
+            // Console.WriteLine($"O resultado da soma é {v1 + v2}");
+            // Console.WriteLine("O resultado da soma é " + (v1 + v2));
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 / v2;
+            Console.WriteLine($"O resultado da divisão é {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 * v2;
+            Console.WriteLine("O resultado da multiplicação é " + (v1 * v2));
+            Console.ReadKey();
+            Menu();
+        }
+    }
 }
