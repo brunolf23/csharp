b60a6b1e2c2633bed8f941971b2c58826a497d6a
Bruno <bruno.lf@hotmail.com>
2024-04-19 19:15:17 -0300

    string v1

---
+++ microsoft/ProjetoTeste7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Aula15();
+        }
+
+        static void Aula0()
+        {
+            var texto = "Testando " + " ";
+            var texto2 = $"Testando {texto}";
+            var texto3 = $"Testando {1 + 1}";
+            var texto4 = @"\Testando \\";
+            Console.WriteLine(texto);
+            Console.WriteLine(texto2);
+            Console.WriteLine(texto3);
+            Console.WriteLine(texto4);
+        }
+
+        static void Aula1()
+        {
+            var texto = "Testando";
+            Console.WriteLine(texto.CompareTo("Testando")); // Retorna 1 - Verdadeiro
+            Console.WriteLine(texto.CompareTo("testando")); // Retorna 0
+            Console.WriteLine(texto.CompareTo("")); // Retorna 0
+            Console.WriteLine(texto.CompareTo(null)); // Retorna 0
+        }
