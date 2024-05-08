3b3c6141f682f4e7a6eb5b5358602b98f16d21b3
Bruno <bruno.lf@hotmail.com>
2024-05-08 20:18:40 -0300

    break

--- microsoft/ProjetoTeste9/Program.cs
+++ microsoft/ProjetoTeste9/Program.cs
@@ -1,4 +1,5 @@
-﻿int[] numbers = { 4, 8, 15, 16, 23, 42 };
+﻿/*
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
 int total = 0;
 bool found = false;
 
@@ -13,3 +14,59 @@ if (found)
     Console.WriteLine("Set contains 42");
 
 Console.WriteLine($"Total: {total}");
+*/
+
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
+{
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
+}
+
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
