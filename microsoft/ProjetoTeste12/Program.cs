3bca4349461bfae67bd224effd2b1f3f8ea36ce6
Bruno <bruno.lf@hotmail.com>
2024-05-14 17:44:32 -0300

    dados na variaveis v2

--- microsoft/ProjetoTeste12/Program.cs
+++ microsoft/ProjetoTeste12/Program.cs
@@ -1,6 +1,28 @@
-﻿Console.WriteLine("Signed integral types:");
+﻿/*
+Console.WriteLine("Tipos integrais assinados:");
 
 Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
 Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
 Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
 Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+Console.WriteLine("");
+Console.WriteLine("Tipos integrais não assinados:");
+
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+Console.WriteLine("");
+Console.WriteLine("Floating point types:");
+Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+*/
+
+int[] data;
+data = new int[3];
+
+string shortenedString = "Hello World!";
+Console.WriteLine(shortenedString);
