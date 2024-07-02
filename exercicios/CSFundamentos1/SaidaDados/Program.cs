36c918d8e19ad932d3bbe5201afca4dd85e665c7
Bruno <bruno.lf@hotmail.com>
2024-07-02 17:17:04 -0300

    formatar saida de dados

---
+++ exercicios/CSFundamentos1/SaidaDados/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Saída de dados: Formatação");
+Console.WriteLine();
+
+int idade = 35;
+string nome = "Bruno";
+
+Console.WriteLine(idade);
+Console.WriteLine(nome);
+
+// Escrever na mesma linha 'Bruno tem 35 anos'
+
+Console.Write(nome);
+Console.Write(" tem ");
+Console.Write(idade);
+Console.Write(" anos.");
+Console.WriteLine("=========================");
+
+// Usar a concatenação usando o operador +
+
+Console.WriteLine(nome + " tem " + idade + " anos.");
+Console.Write(nome + " tem " + idade + " anos.");
+
+Console.ReadLine();
