f6a3ce73de829c0bbcff3833aa58b729a9ebdd98
Bruno <bruno.lf@hotmail.com>
2024-07-04 18:27:44 -0300

    entrada de dados

--- exercicios/CSFundamentos1/EntradaDeDados/Program.cs
+++ exercicios/CSFundamentos1/EntradaDeDados/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("## Entrada de dados ##");
+
+Console.WriteLine("\nInforme o seu nome: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("\nInforme a sua idade: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"\nSeu nome é {nome}");
+Console.WriteLine($", e você tem {idade} anos.");
+
+Console.ReadLine();
