d3cfc29d247c28d386f44123b17c2356a64fb7ce
Bruno <bruno.lf@hotmail.com>
2024-07-08 19:49:35 -0300

    inferencia tipos

--- exercicios/CSFundamentos1/InferenciaTipos/Program.cs
+++ exercicios/CSFundamentos1/InferenciaTipos/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Inferencia de Tipos");
+
+int idade = 35;
+string nome = "Bruno";
+decimal salario = 2500.00m;
+
+Console.WriteLine($"{nome} tem {idade} e recebe {salario}");
+
+// var limitações
+var salario = null;
+var salario;
+var salario, nome, idade;
+
+// não posso mudar o tipo apos inicializar
+var num = 10;
+num = num + 20;
+num = "teste";
+
+Console.ReadKey();
