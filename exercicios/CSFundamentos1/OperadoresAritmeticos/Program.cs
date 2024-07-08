d1f215d758a08c077181abea68b45f7dcfc13b41
Bruno <bruno.lf@hotmail.com>
2024-07-08 17:41:42 -0300

    operadores aritmeticos

---
+++ exercicios/CSFundamentos1/OperadoresAritmeticos/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine(" Operadores Aritméticos ");
+
+Console.WriteLine("Informe o valor de x");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o valor de y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"\nsoma de x + y = {x + y}");
+Console.WriteLine($"subtração de x - y = {x - y}");
+Console.WriteLine($"multiplicação de x * y = {x * y}");
+Console.WriteLine($"divisão de x / y = {x / y}");
+Console.WriteLine($"módulo de x % y = {x % y}");
+
+Console.ReadKey();
