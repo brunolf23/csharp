4af20105ed05bbf9545a3de63cbdd71df0254720
Bruno <bruno.lf@hotmail.com>
2024-07-09 18:35:28 -0300

    constantes

---
+++ exercicios/CSFundamentos1/Constantes/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Constantes");
+
+// calculo da area e perimetro do circulo
+
+double raio, perimetro, area;
+const double pi = 3.14;
+
+Console.WriteLine("Informe o raio do círculo");
+raio = Convert.ToDouble(Console.ReadLine());
+
+perimetro = 2 * Math.PI * raio;
+area = Math.PI * Math.Pow(raio, 2);
+
+Console.WriteLine($"Perímetro = {perimetro}");
+Console.WriteLine($"Área = {area}");
+
+Console.ReadKey();
