e081d4cb6c06b7f341e8fd91ef39e8db192aa348
Bruno <bruno.lf@hotmail.com>
2024-07-10 16:24:07 -0300

    operadores incremento decremento

---
+++ exercicios/CSFundamentos1/OperadoresIncrementoDecremento/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Operadores Incremento e Decremento");
+
+int x = 0;
+
+// pós-decremento -> primeiro resolve depois decrementa
+int resultado1 = x++ + 10;
+
+Console.WriteLine($"pós-incremento => {resultado1}");
+Console.WriteLine($"valor de x => {x}\n");
+
+// pré-decremento => primeiro incrementa depois resolve
+int y = 0;
+
+Console.WriteLine($"y = {y}");
+int resultado2 = ++y + 10;
+
+Console.WriteLine($"pré-incremento => {resultado2}");
+Console.WriteLine($"valor de y => {y}\n");
+
+Console.ReadKey();
