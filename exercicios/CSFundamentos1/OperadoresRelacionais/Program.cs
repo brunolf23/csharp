a7f3ac729dd12cffe38d953dd1788ceb8fadcaef
Bruno <bruno.lf@hotmail.com>
2024-07-16 17:11:23 -0300

    operadores relacionais

---
+++ exercicios/CSFundamentos1/OperadoresRelacionais/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Operadores Relacionais");
+
+Console.WriteLine("Escreva um valor para X:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nEscreva um valor para Y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Se o valor de X é {x}, o valor de Y é {y}, os valores seriam iguais? {x == y}");
+
+Console.ReadKey();
+
+// Operadores Relacionais
+
+// == igualdade
+// > Maior que
+// < Menor que
+// >= Maior ou igual
+// <= menor ou igual
+// != Não igual ou diferente de
