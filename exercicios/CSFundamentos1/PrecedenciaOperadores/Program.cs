f865abe184e4f74b022d864e94d6d2db5f219da8
Bruno <bruno.lf@hotmail.com>
2024-07-17 18:02:18 -0300

    precedencia e associatividade de operadores

---
+++ exercicios/CSFundamentos1/PrecedenciaOperadores/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Precedência e associatividade de operadores");
+
+int x = (5 - 10) * 3;
+Console.WriteLine(x);
+
+
+//Aritméticos: *, /, %, +, -
+//Atribuição: =, *=, /=, %=, +=, -=
+//Incremento/Decremento: ++, --(prefixo)++a, --a
+//                       ++, -- (sufixo) a++, a--
+//Relacionais: <, >, <=, >=, ==, !=
+//Lógicos: !, &&, ||
+
+bool b = (9 != 8) && (5 >= 7 || 8 >= 6);
+Console.WriteLine(b);
+
+Console.ReadKey();
