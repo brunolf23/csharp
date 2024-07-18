835d65d20b5d5a9c38dccf3abb7f8a50a9e0ac09
Bruno <bruno.lf@hotmail.com>
2024-07-18 19:21:01 -0300

    correcao exe 01

---
+++ exercicios/CSFundamentos1/Exe01/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("Exercícios - Fase 1 / Fundamentos");
+
+int idade = 35;
+string nome = "Bruno";
+double altura = 1.83;
+// DateTime data = new DateTime(23/01/1989);
+
+Console.WriteLine($"Meu nome é {nome}, tenho {idade} anos, {altura} de altura e nasci em\n");
+
+// Exercício 5
+const int ano = 12;
+Console.WriteLine(ano);
+
+// Exercício 6
+double? nota = 7.80;
+Console.WriteLine(nota);
+
+// Exercício 8
+int x = 10;
+double numero = 7.99;
+char letra = 'c';
+float temperatura = 27.4f;
+bool ativo = false;
+string nome2 = "Manoel";
+decimal salario = 950.99m;
+DateTime hoje = DateTime.Now;
+
+// Exercício 9
+//v
+//r
+//v
+//v
+//v
+//v
+//r
+
+// Exercício 13
+int u = 77;
+int i = 66;
+
+Console.WriteLine(u+i);
+
+// Exercício 15
+//f
+//f
+//v
+//f
+//f
+//f
+//v
+//v
+//v
+//v
+
+Console.ReadLine();
