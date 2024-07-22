9e6bd0f7c603ca72d651294ded97d8537169c468
Bruno Lopes <bruno.lf@hotmail.com>
2024-07-22 19:08:59 -0300

    estruturas de controle if v2

--- exercicios/EstruturasControle/Estrutura_if/Program.cs
+++ exercicios/EstruturasControle/Estrutura_if/Program.cs
@@ -1,3 +1,35 @@
 ﻿Console.WriteLine("Instrução If");
 
+//Console.Write("Cliente especial (S/N)\t");
+//var resposta = Console.ReadLine().ToUpper();
+
+//if (resposta == "S")
+//{
+//    Console.WriteLine("Desconto de 10%");
+//}
+//else
+//{
+//    Console.WriteLine("Sem desconto!");
+//}
+
+Console.WriteLine("Informe o valor de x");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o valor de y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > y)
+{
+    Console.WriteLine("x é maior que y");
+}
+else
+{
+    if (x < y)
+    Console.WriteLine("x é menor que y");
+
+    else
+    Console.WriteLine("x é igual a y");
+}
+
+Console.WriteLine("Fim do processamento...");
 Console.ReadKey();
---
+++ exercicios/EstruturasControle/Estruturas_if_else/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Instrução if-else\n");
+
+Console.WriteLine("Qual a sua nota da AV?");
+var nota = Convert.ToInt32(Console.ReadLine());
+
+if (nota >= 7.0)
+{
+    Console.WriteLine("Aluno aprovado!");
+}
+else
+{
+    Console.WriteLine("Aluno reprovado!");
+}
+
+Console.WriteLine("Fim do processamento...");
+Console.ReadKey();
