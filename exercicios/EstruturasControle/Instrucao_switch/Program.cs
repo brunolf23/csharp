57a6790ad865029b541dd16bd552a82250670207
Bruno Lopes <bruno.lf@hotmail.com>
2024-07-24 17:47:12 -0300

    estrutura switch v2

--- exercicios/EstruturasControle/Instrucao_switch/Program.cs
+++ exercicios/EstruturasControle/Instrucao_switch/Program.cs
@@ -1,27 +1,39 @@
 ﻿Console.WriteLine("Estrutura switch-case");
 
-Console.WriteLine("Valor total da compra R$: ");
-var compra = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Valor total da compra R$: ");
+//var compra = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Informe o número de parcelas: (1/3)");
-var parcelas = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Informe o número de parcelas: (1/3)");
+//var parcelas = Convert.ToInt32(Console.ReadLine());
 
-switch (parcelas)
+//switch (parcelas)
+//{
+//    case 1:
+//        {
+//            Console.WriteLine($"Prestação R$: 1x {compra / parcelas}");
+//            break;
+//        }
+//    case 2: {
+//            Console.WriteLine($"Prestação R$: 2x {compra / parcelas}");
+//            break;
+//        }
+//    case 3:
+//        {
+//            Console.WriteLine($"Prestação R$: 3x {compra / parcelas}");
+//            break;
+//        }
+//}
+
+Console.WriteLine("Informe um número pra ver se é PAR ou IMPAR");
+var numero = Convert.ToInt32(Console.ReadLine());
+
+switch (numero % 2)
 {
+    case 0:
+        Console.WriteLine($"{numero} é PAR");
+        break;
     case 1:
-        {
-            Console.WriteLine($"Prestação R$: 1x {compra / parcelas}");
-            break;
-        }
-    case 2: {
-            Console.WriteLine($"Prestação R$: 2x {compra / parcelas}");
-            break;
-        }
-    case 3:
-        {
-            Console.WriteLine($"Prestação R$: 3x {compra / parcelas}");
-            break;
-        }
+        Console.WriteLine($"{numero} é IMPAR");
+        break;
 }
-
 Console.ReadKey();
