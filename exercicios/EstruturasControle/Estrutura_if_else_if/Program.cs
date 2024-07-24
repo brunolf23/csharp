0d9bdb696865eb4ec9a36fba0a15abe962837496
Bruno Lopes <bruno.lf@hotmail.com>
2024-07-23 21:24:04 -0300

    estrutura if else if

---
+++ exercicios/EstruturasControle/Estrutura_if_else_if/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Instrução if-else-if");
+
+Console.WriteLine("Informe a nota do aluno: ");
+var nota = Convert.ToDouble(Console.ReadLine());
+
+if (nota < 5)
+{
+    Console.WriteLine("Aluno REPROVADO");
+}
+else if (nota >= 6 && nota <= 9)
+{
+    Console.WriteLine("Aluno APROVADO");
+}
+else if (nota >= 5 && nota < 6)
+{
+    Console.WriteLine("Aluno em RECUPERAÇÃO");
+}
+else
+{
+    Console.WriteLine("Aluno aprovado com DISTINÇÃO");
+}
+Console.WriteLine("Fim do PROCESSAMENTO....");
+
+Console.ReadKey();
