e7b97cd934e165f1421521d6590b41b72269b32e
Bruno Lopes <bruno.lf@hotmail.com>
2024-07-26 20:09:29 -0300

    estrutura switch 2

---
+++ exercicios/EstruturasControle/Estrutura_switch2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Estrutura switch-case 2");
+
+var mes = Console.ReadLine().ToLower();
+
+switch (mes)
+{
+    case "janeiro":
+    case "março":
+    case "maio":
+    case "julho":
+    case "agosto":
+    case "outubro":
+    case "dezembro":
+        Console.WriteLine("Este mês tem 31 dias");
+        break;
+    case "fevereiro":
+        Console.WriteLine("Este mês tem 28 ou 29 dias");
+        break;
+    default:
+        Console.WriteLine("Este mês tem 30 dias");
+        break;
+}
+Console.WriteLine("Fim do processamento...");
+
+Console.ReadKey();
