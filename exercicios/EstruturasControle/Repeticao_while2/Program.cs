ce5a5d3b768d9ea23b05a46f1ad29ae23765174d
Bruno Lopes <bruno.lf@hotmail.com>
2024-07-29 17:40:25 -0300

    repeticao while 2

---
+++ exercicios/EstruturasControle/Repeticao_while2/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Instrução While");
+
+//while (true)
+//{
+//    Console.WriteLine("Informe um número inteiro: (Para sair digite 999)");
+
+//    int numero = Convert.ToInt32(Console.ReadLine());
+
+//    if (numero == 999)
+//    {
+//        break;
+//    }
+//    if (numero % 2 == 0)
+//    {
+//        Console.WriteLine($"O número {numero} é PAR.\n");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"O número {numero} é IMPAR.\n");
+//    }
+//}
+
+int x = 1;
+
+while (x <= 5)
+{
+    int y = 1;
+    while (y <= 5)
+    {
+        Console.WriteLine($"({x},{y}) ");
+        y++;
+    }
+    x++;
+    Console.WriteLine();
+}
+
+Console.WriteLine("\nFim de processamento...");
+
+Console.ReadKey();
