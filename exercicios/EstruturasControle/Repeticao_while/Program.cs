d1c30020e1bb4e0e894cadf00c4ac39df231f5e9
Bruno Lopes <bruno.lf@hotmail.com>
2024-07-29 15:20:48 -0300

    repeticao while

---
+++ exercicios/EstruturasControle/Repeticao_while/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Instrução While");
+
+int numero;
+var contador = 1;
+
+Console.WriteLine("Digite um número maior que zero");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero > 0)
+{
+    Console.WriteLine($"Tabuada do número {numero}.\n");
+    while (contador < 11)
+    {
+        Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+        contador++;
+    }
+}
+else
+{
+    Console.WriteLine("Número deve ser maior que zero!");
+}
+Console.WriteLine("Fim do processamento...");
+
+Console.ReadKey();
