3f45c27f5285d5d547f32480dfd89c44865e1bfb
Bruno Lopes <bruno.lf@hotmail.com>
2024-07-30 15:44:36 -0300

    repeticao for

---
+++ exercicios/EstruturasControle/Repeticao_for/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Instrução for\n");
+
+double numero, resultado = 0;
+double i;
+
+Console.WriteLine("Informe o número inteiro maior que zero: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero > 0)
+{
+    for (i = 1; i <= 10; i++)
+    {
+        resultado = numero * i;
+        Console.WriteLine($"{numero} x {i} = {resultado}");
+    }
+}
+else
+{
+    Console.WriteLine("Número inválido");
+}
+
+Console.WriteLine("\nFim do processamento...");
+
+Console.ReadKey();
---
+++ exercicios/EstruturasControle/Repeticao_while3/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Instrução While\n");
+
+//var i = 1;
+
+//do
+//{
+//    Console.WriteLine($"i = {i}");
+//    i++;
+//}
+//while ( i <= 10 );
+
+// exemplo 2
+
+int x = 0;
+
+do
+{
+    int y = 0;
+    do
+    {
+        Console.Write($"{x},{y} ");
+        y++;
+    }
+    while (y < 5);
+    x++;
+    Console.WriteLine();
+}
+while (x < 5);
+
+Console.ReadKey();
