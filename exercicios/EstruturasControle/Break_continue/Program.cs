655ee2302b32b1da1d160712bc4e22b4a5455204
Bruno Lopes <bruno.lf@hotmail.com>
2024-07-31 18:16:48 -0300

    break e continue

---
+++ exercicios/EstruturasControle/Break_continue/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Break e Continue\n");
+
+//int x = 1;
+
+//while (x <= 10)
+//{
+//    if (x == 5)
+//    {
+//        x++;
+//        continue;
+//    }
+//    Console.WriteLine($"x = {x}");
+//    x++;
+//}
+
+// segundo exemplo
+
+for (int x = 1; x <= 10; x++)
+{
+    if (x == 5)
+        continue;
+
+    if (x == 8)
+        break;
+
+    Console.WriteLine($"{x}");
+}
+
+Console.WriteLine("\nFim do processamento...");
+
+Console.ReadKey();
